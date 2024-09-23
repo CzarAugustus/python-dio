82122fa86dfb62c3552eadcaca0c27fec3cd029e
CzarAugustus <caugustosl@yahoo.com.br>
2024-09-23 16:26:16 -0300

    commit de variaveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "Guilherme"
+idade = 28
+
+nome = ("Giovana")
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_SATATES = ["AC", "AM", "AP", "AL", "BA", "CE", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+                      "PB", "PI", "PR", "PE", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
+
+print(BRAZILIAN_SATATES)
