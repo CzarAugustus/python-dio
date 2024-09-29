50a7a65d4dadf3d23ab267f0151ea2fdc8f9027d
CzarAugustus <caugustosl@yahoo.com.br>
2024-09-28 21:55:07 -0300

    aprendendo sobre string em python

---
+++ strig_4.py
@@ -0,0 +1,22 @@
+nome = "Cesar"
+
+mensagem = f"""
+    Olá, meu nome é {nome},
+Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recursos.
+"""
+print(mensagem)
+
+print(
+    """
+    *********** MENU ***********
+
+    1 - Depositar
+    2 - Sacar
+    3 - Saldo
+    4 - Sair
+    
+    ****************************
+        Obrigdo por utilizar nossos sistemas!
+    """
+)
